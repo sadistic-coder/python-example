3edf0542b4a4014f2a23948da5eaefa4abe5a1f7
Las-Wonho <haho013039@gmail.com>
2020-01-15 13:12:13 +0900

    Implement Todo Domain Entity

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -1,5 +1,11 @@
 from todo import __version__
+from todo.entity import Todo
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_create_todo():
+    todo = Todo("test todo", "just test instance")
+    assert todo is not None
---
+++ todo/entity.py
@@ -0,0 +1,12 @@
+class Todo:
+    def __init__(self, name: str, description: str):
+        self._name = name
+        self._description = description
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def description(self) -> str:
+        return self._description
